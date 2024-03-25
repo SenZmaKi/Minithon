6a4a15c657111f2b376fd37f160bc7d54fd34f51
Sen ZmaKi <phidelalvin@gmail.com>
2024-03-25 21:15:28 +0300

    Add printing runtime after lexer test run

--- minithon/lexer/test.py
+++ minithon/lexer/test.py
@@ -1,6 +1,7 @@
 from .main import tokenize
 from pprint import pprint
 from pathlib import Path
+import time
 
 CURR_ROOT_DIR = Path(__file__).parent
 
@@ -8,8 +9,11 @@ CURR_ROOT_DIR = Path(__file__).parent
 def main() -> None:
     with open(CURR_ROOT_DIR / "test.mipy") as f:
         contents = f.read()
+        start_time = time.time()
         tokens = tokenize(contents)
+        runtime = time.time() - start_time
         pprint(tokens)
+        print(f"Runtime: {runtime:.4f} seconds")
 
 
 if __name__ == "__main__":
